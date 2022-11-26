b322265aa781b43ad64d1e12bf9164ad65ec9d48
Petit_Homme Wadly <wadleyp96@gmail.com>
2022-11-26 06:14:34 -0800

    Update bd_config

--- data/bd_config.php
+++ data/bd_config.php
@@ -1,7 +1,7 @@
 <?php
-$bd_dns ='mysql:host=localhost;dbname=u671139698_dressupstock';
-$bd_username = 'u671139698_dressup';
-$bd_pass = '1234@Dressup';
+$bd_dns ='mysql:host=localhost;dbname=dressup';
+$bd_username = 'root';
+$bd_pass = '';
 $bd_options =[
     PDO::MYSQL_ATTR_INIT_COMMAND => 'set NAMES utf8',
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
